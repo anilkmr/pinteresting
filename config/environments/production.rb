afaaf5ba2157b16fed45adf9f593dd4b5a6d8474
Anil Kumar <akumar05t@gmail.com>
2013-12-28 21:10:25 -0500

    Devise configurations for log in and sign up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+   # Devise specific configurations
+  config.action_mailer.default_url_options = { :host => 'http://akapp.herokuapp.com/' }
 end
